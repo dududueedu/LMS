e6bfca9e39114e36960af901bd8050dbd80b6aa8
Eduardo <eduardooffside@gmail.com>
2021-01-15 23:15:36 -0300

    atividades

---
+++ ATV03/anotation.py
@@ -0,0 +1,8 @@
+por padrão o flex tenta botar todos os elementos na mesma linha
+
+flex-direction (eixo princimal)
+    row, row-reverse, column, column-reverse
+
+flex-wrap
+    aqui pode mudar
+    nowrap, wrap, wrap-reverse
