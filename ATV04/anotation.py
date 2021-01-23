b0e111f6ce1a1d1629bb8b7880efaded91a681f4
Eduardo <eduardooffside@gmail.com>
2021-01-23 16:25:03 -0300

    atividades

--- ATV04/anotation.py
+++ ATV04/anotation.py
@@ -1,3 +1,12 @@
 bordas arredondadas (border-radius)
 sombras (text-shadow) - textos
 (box-shadow) - bloco.
+
+transformações 2D:
+    translate() - recebe dois parametros;
+    rotate() - rotaciona em graus;
+    scale() - redimenciona o tamanho;
+    skewX() - deformar em relação ao eixo X;
+    skewY() - deformar em relação ao eixo Y;
+    matrix() - JUNTA TODAS AS FUNÇÕES.
+
