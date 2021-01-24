45cd6c2b2c4bd4a3669c8438a364115b0deee505
Eduardo <eduardooffside@gmail.com>
2021-01-24 16:58:07 -0300

    atividades

--- ATV04/anotation.py
+++ ATV04/anotation.py
@@ -10,3 +10,31 @@ transformações 2D:
     skewY() - deformar em relação ao eixo Y;
     matrix() - JUNTA TODAS AS FUNÇÕES.
 
+transições
+    onde o efeito vai ser adicionado, direção, lado;
+    duração dele, em segundos;
+    transition-timing-function: define a curva de velocidade de transição;
+    transition-delay;
+
+    apenas transition: (4 valores acima);
+
+
+Animação:
+    @keyframes;
+
+    @keyframes nomeX{
+        from{elementos de entrada}
+        to{elementos de saída}
+    }
+
+    * no css, tem que chamar a Animação:
+        animation-name: nomeX;
+        animation-duration: duração da Animação;
+
+    também é possível fazer a Animação em porcentagem:
+        ex: em 0%: faça isso;
+            em 25%: faça isso;
+            em 50%: faça isso;
+            em 100%: faça isso;
+    
+    animation-interation-count: 3; //repete 3x
