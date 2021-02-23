f22160f2feef85dc3bf03872809f9b6bd3b78060
Eduardo <eduardooffside@gmail.com>
2021-02-23 15:00:51 -0300

    atividades

--- ATV05/REDE_SOCIAL/javas.js
+++ ATV05/REDE_SOCIAL/javas.js
@@ -19,3 +19,25 @@ meu_botao.addEventListener("click", function(){
 //botao postar
 let botaopostar = document.querySelector(".navegador_superior .meu_botao_postar")
 console.log(botaopostar)
+
+//OVERLAY com o botao postar
+let overlayModal = document.querySelector(".overlay_modal")
+
+botaopostar.addEventListener("click", function(){
+    overlayModal.classList.add("active")
+})
+
+//botões
+let botao_close = document.querySelector(".overlay_modal .container_modal .footer .botao_close")
+botao_close.addEventListener("click", function(){
+    overlayModal.classList.remove("active")
+})
+
+let botaoX = document.querySelector(".overlay_modal .container_modal .header_modal .botaoX")
+botaoX.addEventListener("click", function(){
+    overlayModal.classList.remove("active")
+})
+
+window.addEventListener("click", function(event){
+    if(event.target == overlayModal) overlayModal.classList.remove("active")
+})
