52202717e68e9292891077209e3b1d2fefcc5c37
Eduardo <eduardooffside@gmail.com>
2021-03-21 21:53:46 -0300

    atividades

--- ATV06/PapoWeb/javascript/script.js
+++ ATV06/PapoWeb/javascript/script.js
@@ -1,2 +1,13 @@
+/*
+GET – Solicita algum recurso: 
+    Dados são anexados à URL, ficando visíveis ao usuário.
+POST – Envia dados referentes ao recurso especificado para serem processados: 
+    Dados são incluídos no corpo do comando.
+PUT – Envia certo recurso: Em geral, é usado para atualizar dados
+DELETE – Exclui o recurso
+HEAD – Variação do GET em que o recurso não é retornado: 
+    Usado para obter metainformações por meio do cabeçalho da resposta, sem ter que recuperar todo o conteúdo.
+*/
+
 let myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
 myModal.show()
