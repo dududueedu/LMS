b25697dac128c84b95088ef224282a4cf11f953b
Eduardo <eduardooffside@gmail.com>
2021-03-26 18:09:59 -0300

    axios-JS

--- ATV06/PapoWeb/javascript/script.js
+++ ATV06/PapoWeb/javascript/script.js
@@ -9,6 +9,62 @@ HEAD – Variação do GET em que o recurso não é retornado:
     Usado para obter metainformações por meio do cabeçalho da resposta, sem ter que recuperar todo o conteúdo.
 */
 
+//mostra modal
 let myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
 myModal.show()
 
+let pag = document.querySelector(".container .pagina")
+//estrutura da lista de grupos
+function listarGrupos(nome) {
+    group_list = document.querySelector(".group-list")
+
+    let grupo = document.createElement("div")
+    grupo.classList.add("grupo");
+    grupo.textContent = nome;
+
+    group_list.appendChild(grupo);
+    return group_list;
+}
+
+function GETGroups() {
+            axios({
+                method: 'GET',
+                url: 'https://server-json-lms.herokuapp.com/grupos'
+            }).then(
+                (response) => {
+                    let grupos = response.data;
+                    for (let group of grupos) {
+                        let createGroup = listarGrupos(group.nome);
+                        pag.appendChild(createGroup);
+                        console.log(group.nome);
+                    }
+                }
+            ).catch(
+                (err) => {
+                    console.log(err);
+                }
+            )
+        }
+
+GETGroups();
+
+let navbarnome = document.querySelector(".pagina .header_nome #navbarText.navbar-collapse");
+console.log(navbarnome)
+
+globalName = ""
+
+    let form = document.getElementById('form_modal');
+    let input = document.getElementById('nome');
+
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        globalName = input.value;
+        console.log(input.value);
+        let span = document.querySelector(".navbar-text");
+        span.textContent = globalName;
+
+        console.log(span);
+        navbarnome.appendChild(span);
+        
+        myModal.hide()
+    });
