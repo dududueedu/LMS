e0e951617a661eea235be459d16fce8b53196c8b
Eduardo <eduardooffside@gmail.com>
2021-03-27 00:33:49 -0300

    axios-JS

--- ATV06/PapoWeb/javascript/script.js
+++ ATV06/PapoWeb/javascript/script.js
@@ -16,9 +16,9 @@ myModal.show()
 let pag = document.querySelector(".container .pagina")
 //estrutura da lista de grupos
 function listarGrupos(nome) {
-    group_list = document.querySelector(".group-list")
+    group_list = document.querySelector(".group-list");
 
-    let grupo = document.createElement("div")
+    let grupo = document.createElement("div");
     grupo.classList.add("grupo");
     grupo.textContent = nome;
 
@@ -27,44 +27,63 @@ function listarGrupos(nome) {
 }
 
 function GETGroups() {
-            axios({
-                method: 'GET',
-                url: 'https://server-json-lms.herokuapp.com/grupos'
-            }).then(
-                (response) => {
-                    let grupos = response.data;
-                    for (let group of grupos) {
-                        let createGroup = listarGrupos(group.nome);
-                        pag.appendChild(createGroup);
-                        console.log(group.nome);
-                    }
-                }
-            ).catch(
-                (err) => {
-                    console.log(err);
-                }
-            )
+    axios({
+        method: 'GET',
+        url: 'https://server-json-lms.herokuapp.com/grupos'
+    }).then(
+        (response) => {
+            let grupos = response.data;
+            for (let group of grupos) {
+                let createGroup = listarGrupos(group.nome);
+                pag.appendChild(createGroup);
+                console.log(group.nome);
+            }
         }
-
+    ).catch(
+        (err) => {
+            console.log(err);
+        }
+    )
+}
 GETGroups();
 
+//criar grupos
+let form_group = document.querySelector("#new-group");
+let input_form = document.querySelector("#new-group #grupo");
+console.log(form_group)
+
+form_group.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    axios({
+        method: "POST",
+        url: "https://server-json-lms.herokuapp.com/grupos",
+        data: {
+            nome: input_form.value   
+        }
+    }).then((response) => {
+        GETGroups();
+    }).catch((error) => {
+        console.log(error);
+    })
+})
+
+//nome do usuário
 let navbarnome = document.querySelector(".pagina .header_nome #navbarText.navbar-collapse");
 console.log(navbarnome)
 
 globalName = ""
+let form = document.getElementById('form_modal');
+let input = document.getElementById('nome');
 
-    let form = document.getElementById('form_modal');
-    let input = document.getElementById('nome');
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    globalName = input.value;
+    console.log(input.value);
+    let span = document.querySelector(".navbar-text");
+    span.textContent = globalName;
 
-    form.addEventListener('submit', (e)=>{
-        e.preventDefault();
-        globalName = input.value;
-        console.log(input.value);
-        let span = document.querySelector(".navbar-text");
-        span.textContent = globalName;
-
-        console.log(span);
-        navbarnome.appendChild(span);
+    console.log(span);
+    navbarnome.appendChild(span);
         
-        myModal.hide()
-    });
+    myModal.hide()
+});
